b745aaf30c025fc03b6661ac6b6867bb0566b1ed
PeterK <peter.kmecl@fe.uni-lj.si>
2024-09-19 12:33:11 +0200

    Prvi commit

---
+++ scripts/primer1.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+import time
+
+
+def main():
+    print("Hello world!")
+
+if __name__ == "__main__":
+    main()
---
+++ scripts/primer2.py
@@ -0,0 +1,5 @@
+#! /usr/bin/python3
+import time
+import primer1
+
+print("Hi again :)")
